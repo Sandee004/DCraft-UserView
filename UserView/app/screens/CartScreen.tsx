53b7c09c1dbb997e4f1582d700a7e1876b371cb3
Sandra <oyarekhuas@gmail.com>
2025-06-27 18:44:21 +0100

    Finished temporarily

--- UserView/app/screens/CartScreen.tsx
+++ UserView/app/screens/CartScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useCart } from "../context/CartContext";
 import CartItem from "./CartItem";
 import tw from "twrnc";
@@ -7,6 +7,10 @@ import tw from "twrnc";
 export default function CartScreen() {
   const { cartItems } = useCart();
   const total = cartItems.reduce((sum, p) => sum + p.price * p.quantity, 0);
+
+  const proceedtoCheckout = () => {
+    console.log("Coming soon....");
+  };
 
   return (
     <View style={tw`flex-1 p-4 bg-white`}>
@@ -19,9 +23,20 @@ export default function CartScreen() {
             renderItem={({ item }) => <CartItem product={item} />}
           />
 
-          <Text style={tw`text-right text-lg font-bold mt-4`}>
-            Total: ${total.toFixed(2)}
-          </Text>
+          <View style={tw`flex-row justify-between mb-4`}>
+            <Text style={tw`text-right text-lg font-bold mt-4`}>Total:</Text>
+            <Text style={tw`text-right text-lg font-bold mt-4`}>
+              ${total.toFixed(2)}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={proceedtoCheckout}
+            style={tw`w-full bg-[#000080] py-3 rounded-md justify-center items-center`}
+          >
+            <Text style={tw`text-white text-center font-medium text-lg`}>
+              Proceed to Checkout
+            </Text>
+          </TouchableOpacity>
         </>
       ) : (
         <Text style={tw`text-center text-gray-500 mt-10`}>
